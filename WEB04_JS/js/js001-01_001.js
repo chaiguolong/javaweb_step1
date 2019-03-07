58943c42ccc273575b0e622867df5cf82b24fed2
只要你过得比我好 <584238433@qq.com>
2019-03-07 21:17:40 +0800

    完成案例

---
+++ WEB04_JS/js/js001-01_001.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+	//1.获取表格
+	var tblEle = document.getElementById("tbl");
+	//2.获取表格中tbody里面的行数(长度)
+	var len = tblEle.tBodies[0].rows.length;
+	//3.对tbody里面的行进行遍历
+	for(var i=0;i<len;i++){
+		if(i%2==0){
+			//4.对偶数行设置背景颜色
+			tblEle.tBodies[0].rows[i].style.backgroundColor='pink';
+		}else{
+			//5.对奇数行设置背景颜色
+			tblEle.tBodies[0].rows[i].style.backgroundColor="gold";
+		}
+	}
+}
---
+++ WEB04_JS/js/js001-02_002.js
@@ -0,0 +1,7 @@
+function changeColor(id,flag){
+	if(flag == 'over'){
+		document.getElementById(id).style.backgroundColor = "red";
+	}else if(flag == 'out'){
+		document.getElementById(id).style.backgroundColor = "white";
+	}
+}
