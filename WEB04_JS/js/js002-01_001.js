b0ef77bcffbdbcb91dbc62f314b63e7797238c4c
只要你过得比我好 <584238433@qq.com>
2019-03-08 10:14:01 +0800

    案例二完成学习

---
+++ WEB04_JS/js/js002-01_001.js
@@ -0,0 +1,21 @@
+function checkAll(){
+	//1.获取编号前面的复选框
+	var checkAllEle = document.getElementById("checkAll");
+	//2.对编号前面复选框的状态进行判断
+	if(checkAllEle.checked==true){
+		//3.获取下面所有的复选框
+		var checkOnes = document.getElementsByName("checkOne");
+		//4.对获取的所有复选框进行遍历
+		for(var i=0;i<checkOnes.length;i++){
+			checkOnes[i].checked=true;
+		}
+	}else{
+		//6.获取下面所有的复选框
+		var checkOnes = document.getElementsByName("checkOne");
+		//7.对获取的所有复选框进行遍历
+		for(var i=0;i<checkOnes.length;i++){
+			//8.拿到每一个复选框，并将其状态置为未选中
+			checkOnes[i].checked = false;
+		}
+	}
+}
---
+++ WEB04_JS/js/js002-02_003.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+	document.getElementById("btn").onclick = function(){
+		//获取ul元素节点
+		var ulEle = document.getElementById("ul1");
+		//创建城市文本节点
+		var textNode = document.createTextNode("深圳");
+		//创建li元素节点
+		var liEle = document.createElement("li");
+		//4.将城市文本节点添加到li元素节点中去
+		liEle.appendChild(textNode);
+		//5.将li添加到ul中去
+		ulEle.appendChild(liEle);
+	}
+}
